cfa94f8ca9fe31de4011be89991d1a5869787a2c
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-12 19:59:49 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,11 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
