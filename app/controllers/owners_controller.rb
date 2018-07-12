8a9ba242df0b60963c019ed3124e4d3aeb08f4c9
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-12 18:52:38 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,9 +11,13 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-    @owner = Owner.create(params[:owner])
-    redirect "owners/#{@owner.id}"
+  @owner = Owner.create(params[:owner])
+  if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
   end
+  @owner.save
+  redirect to "owners/#{@owner.id}"
+end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
